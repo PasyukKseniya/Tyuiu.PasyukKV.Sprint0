a6ce9bc4834273a520cb80159b2f493ce4e5ade6
Tyuiu.PasyukKV@outlook.com <Tyuiu.PasyukKV@outlook.com>
2023-10-01 16:12:44 +0500

    Task5

---
+++ Tyuiu.PasyukKV.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PasyukKV.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PasyukKV.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c5d0f442-c950-427e-8934-1abe97e28251")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.PasyukKV.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.PasyukKV.Sprint0.Task5.V0/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tyuiu.PasyukKV.Sprint0.Task5.V0.Lib;
 namespace Tyuiu.PasyukKV.Sprint0.Task5.V0
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("A + B = " + DataService.Addition(10, 5));
+            Console.WriteLine("A - B = " + DataService.Substract(10, 5));
+            Console.WriteLine("A * B = " + DataService.Mplicate(10, 5));
+            Console.WriteLine("A / B = " + DataService.Division(10, 0));
+            Console.ReadKey();
         }
     }
 }
