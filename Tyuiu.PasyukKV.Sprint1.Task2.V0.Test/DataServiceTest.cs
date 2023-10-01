43652ef31c32a5954e7264482b0944259de51add
Tyuiu.PasyukKV@outlook.com <Tyuiu.PasyukKV@outlook.com>
2023-10-01 15:44:53 +0500

    Task2

---
+++ Tyuiu.PasyukKV.Sprint1.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PasyukKV.Sprint1.Task2.V0.Lib;
+namespace Tyuiu.PasyukKV.Sprint1.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+                {
+                    var name = "Ксения";
+                    var res = DataService.GetMessage(name);
+
+                    Assert.AreEqual("Привет, Ксения", res);
+                }
+        }
+    }
+}
