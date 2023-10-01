f55cd46ec1051e52f5fc57d2d8e7b0b584fbf991
Tyuiu.PasyukKV@outlook.com <Tyuiu.PasyukKV@outlook.com>
2023-10-01 15:49:39 +0500

    Task3

---
+++ Tyuiu.PasyukKV.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PasyukKV.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+         
+        public static int summ(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
+
---
+++ Tyuiu.PasyukKV.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PasyukKV.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PasyukKV.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("24d3aaf0-36df-4683-874f-4530468efeae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
